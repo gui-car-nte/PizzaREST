0d27b80c4b7d9c9b749f257ab45a6053471d7d40
guillermo.carrillo1 <guillermo.carrillo@nter.es>
2024-08-06 18:46:21 +0200

    controller and service finalization

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,19 +1,21 @@
 from sqlalchemy.orm import Session
-from app.models import user_schema as user_model
-from app.models import user_model as user_schema
-from app.services.auth_service import get_password_hash
+from app.models import user_model, user_schema
 
-def create_user(db: Session, user: user_schema.UserCreate):
-    hashed_password = get_password_hash(user.password)
-    db_user = user_model.User(
+def create_user(db: Session, user: user_model.UserCreate):
+    db_user = user_schema.User(
         username=user.username,
-        hashed_password=hashed_password,
         email=user.email,
         phone_number=user.phone_number,
-        address=user.address
+        address=user.address,
+        password=user.password,
     )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(user_schema.User).filter(user_schema.User.username == username).first()
+    if user and user.password == password: # type: ignore # TODO check what is causing this warning
+        return user
+    return None
